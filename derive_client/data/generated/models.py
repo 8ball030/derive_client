ffb51f7642cfbc3b48247ee5a5a3879b305894e9
zarathustra <zarathustra_git@pm.me>
2025-10-30 19:20:02 +0100

    chore: generate-models

--- derive_client/data/generated/models.py
+++ derive_client/data/generated/models.py
@@ -1,10 +1,8 @@
 # ruff: noqa: E741
 from __future__ import annotations
-
 from decimal import Decimal
 from enum import Enum
 from typing import Any, Dict, List, Optional, Union
-
 from msgspec import Struct
 
 
@@ -1318,10 +1316,34 @@ class PublicGetTransactionParamsSchema(Struct):
     transaction_id: str
 
 
+class TransactionDataInner(Struct):
+    asset: str
+    amount: str
+    decimals: int
+
+
+class TransactionData(Struct):
+    data: TransactionDataInner
+    nonce: int
+    owner: str
+    expiry: int
+    module: str
+    signer: str
+    asset_id: str
+    signature: str
+    asset_name: str
+    subaccount_id: int
+    is_atomic_signing: bool
+
+
+class TransactionErrorLog(Struct):
+    error: str
+
+
 class PublicGetTransactionResultSchema(Struct):
-    data: dict
+    data: TransactionData
     status: TxStatus
-    error_log: Optional[dict] = None
+    error_log: Optional[TransactionErrorLog] = None
     transaction_hash: Optional[str] = None
 
 
