0c8ae01f929b2bab764c5b4146b91ce37627238f
zarathustra <zarathustra_git@pm.me>
2025-11-05 12:36:41 +0100

    chore: remove unused template: transactions_structs.py

--- scripts/generate-private-key.py
+++ scripts/generate-private-key.py
@@ -1,14 +1,11 @@
-"""
-Helpful script to create a new ethereum key pair.
-"""
+"""Script to create a new ethereum key pair."""
 
 from eth_account import Account
 
 
 def main():
-    """
-    Create a new ethereum key pair and print it to the console.
-    """
+    """Create a new ethereum key pair and print it to the console."""
+
     account = Account.create()
     print(f"Address: {account.address}")
     print(f"Private Key: {account.key.hex()}")
