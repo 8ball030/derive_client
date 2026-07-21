b245753fd1ac4c34ed81d103480825a1bb0dbee3
8ball030 <8baller@station.codes>
2026-07-21 14:52:37 +0100

    chore:bumped-shell.nix

--- shell.nix
+++ shell.nix
@@ -1,13 +1,5 @@
 with import <nixpkgs> {};
 
-let
-  python = pkgs.python311;
-
-  poetry = pkgs.poetry.override {
-    python3 = python;
-  };
-in
-
 mkShell {
   NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
     nss
@@ -28,18 +20,14 @@ mkShell {
   ];
 
   buildInputs = [
-    pkgs.cowsay
-    pkgs.gum
-    pkgs.asciinema
-    pkgs.asciinema-agg
-    pkgs.poetry
     pkgs.python311
-    pkgs.python311Packages.pip
     pkgs.python311Packages.virtualenv
-    pkgs.python311Packages.unicurses
-    pkgs.python311Packages.gnureadline
-    pkgs.python311Packages.pyopenssl
-    pkgs.python311Packages.cython
+    pkgs.poetry
+    # pkgs.python311Packages.pip
+    # pkgs.python311Packages.unicurses
+    # pkgs.python311Packages.gnureadline
+    # pkgs.python311Packages.pyopenssl
+    # pkgs.python311Packages.cython
     pkgs.python311Packages.cytoolz
     pkgs.pythonManylinuxPackages.manylinux2014Package
     pkgs.cmake
@@ -51,8 +39,20 @@ mkShell {
     set -e
     export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:${pkgs.pythonManylinuxPackages.manylinux2014Package}/lib:$LD_LIBRARY_PATH";
     echo 'Spinning up Python Virtual Environment in .nix-venv directory 🐍'
-    ${pkgs.python311.interpreter} -m venv .nix-venv
-    export PATH=$PWD/.nix-venv/bin:$PATH
+    # ${pkgs.python311.interpreter} -m venv .nix-venv
+    # export PATH=$PWD/.nix-venv/bin:$PATH
+
+    # echo "Python version: $(which python) $(python --version)"
+    # echo $(pip show cytoolz)
+
+    
+    # Check if poetry is installed
+    if ! command -v poetry &> /dev/null; then
+      echo 'Installing poetry 🐍'
+      .nix-venv/bin/pip install poetry==2.0.1 poetry-core
+    fi
+
+    # Ensure virtual environment dependencies
   '';
 }
 
